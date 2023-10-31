527db70c0c04bee5aedb8da65900520c4f7eb4aa
chenchongli <1123752904@qq.com>
2023-10-31 10:38:12 +0800

    int -> char char -> int

--- demo_test/src/main/java/com/always/demo/test/Main1.java
+++ demo_test/src/main/java/com/always/demo/test/Main1.java
@@ -7,12 +7,22 @@ public class Main1 {
         char a = 'a';
         char z = 'z';
 
+        char zero = '0';
+        char one = '1';
+        char two = '2';
+        char nine = '9';
+
         // char -> int
         System.out.println((int) A);    // 65
         System.out.println((int) Z);    // 90
         System.out.println((int) a);    // 97
         System.out.println((int) z);    // 122
-
+        System.out.println("----------");
+        System.out.println((int) zero);    // 48
+        System.out.println((int) one);    // 49
+        System.out.println((int) two);    // 50
+        System.out.println((int) nine);    // 57
+        System.out.println("----------");
         // int -> char
         int x = 65;
         char charx = (char) x;
